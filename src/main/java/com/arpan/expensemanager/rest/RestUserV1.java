90c172b74ba957e3772d819952231ff823e2ae7a
arpangill90 <arpangill90@gmail.com>
2024-03-19 06:09:51 -0400

    Add basic end points

--- src/main/java/com/arpan/expensemanager/rest/RestUserV1.java
+++ src/main/java/com/arpan/expensemanager/rest/RestUserV1.java
@@ -1,10 +1,27 @@
 package com.arpan.expensemanager.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.arpan.expensemanager.data.dto.UserDto;
+import com.arpan.expensemanager.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user/")
+@RequiredArgsConstructor
 public class RestUserV1 {
 
+    private final UserService userService;
+
+    @GetMapping("user/{id}")
+    public ResponseEntity<UserDto> retrieveUser(@PathVariable long id) {
+            return new ResponseEntity<>(userService.retrieveUser(id), HttpStatus.OK);
+    }
+
+    @PostMapping("user")
+    public ResponseEntity<UserDto> addUser(@RequestBody @Valid UserDto user) {
+        return new ResponseEntity<>(userService.addUser(user), HttpStatus.OK);
+    }
 }

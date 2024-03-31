93fd0ca84e7a455d7f47bc6d0e9892bd4bc82e21
arpangill90 <arpangill90@gmail.com>
2024-03-30 22:19:31 -0400

    Update category API

--- src/main/java/com/arpan/expensemanager/rest/RestCategoryV1.java
+++ src/main/java/com/arpan/expensemanager/rest/RestCategoryV1.java
@@ -1,12 +1,28 @@
 package com.arpan.expensemanager.rest;
 
+import com.arpan.expensemanager.data.dto.CategoryDto;
+import com.arpan.expensemanager.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/category/")
+@RequiredArgsConstructor
 public class RestCategoryV1 {
 
+    private final CategoryService categoryService;
+
+    @GetMapping("{user}")
+    public ResponseEntity<List<CategoryDto>> getAllExpenses(@PathVariable long user) {
+        return new ResponseEntity<>(categoryService.findCategoriesForUser(user), HttpStatus.OK);
+    }
 
 
 }

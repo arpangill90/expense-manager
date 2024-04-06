082caa9759914d67aa31d6e7879d4b2afd6f0822
arpangill90 <arpangill90@gmail.com>
2024-04-06 10:02:31 -0400

    Initializing integration test

---
+++ src/test/java/com/arpan/expensemanager/rest/RestUserV1IT.java
@@ -0,0 +1,13 @@
+package com.arpan.expensemanager.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RestUserV1IT {
+
+    @Test
+    void retrieveUser() {
+    }
+}
